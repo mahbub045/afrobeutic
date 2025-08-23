1d439dc65e1f688063f5b8886b19dd2bf5627319
mahbub045 <mrahman172014@bscse.uiu.ac.bd>
2025-08-23 21:59:15 +0600

    Add theme support with next-themes and implement light/dark mode toggle

---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+const Test: React.FC = () => {
+  return (
+    <div className="bg-green-600 dark:bg-red-500">
+      <h2 className=" text-red-500 dark:text-green-500">
+        Welcome to Afrobeutic
+      </h2>
+    </div>
+  );
+};
+
+export default Test;
